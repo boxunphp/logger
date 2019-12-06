2d08921f94fc4cfcf20de958c07a917a45c85d5a
Jordy <arno.zheng@gmail.com>
2019-12-06 15:03:11 +0800

    init: Logger类

---
+++ tests/LoggerTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jordy
+ * Date: 2019/12/6
+ * Time: 2:56 PM
+ */
+
+namespace LoggerTest;
+
+use PHPUnit\Framework\TestCase;
+
+class LoggerTest extends TestCase
+{
+
+}
